18c03a92086decb9bd1dc419deb46587a22e9963
Bheshaj Singh Shekhawat <bheshaj.shekhawat@maersk.com>
2022-03-14 02:36:42 +0530

    Project 1st commit

---
+++ cart.php
@@ -0,0 +1,115 @@
+<?php
+  require("includes/common.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Cart | Life Style Store</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap Core CSS -->
+          <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+          <link href="./css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+          <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+          <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body style="padding-top: 50px;">
+
+        <!-- Header -->
+        <?php
+        include 'includes/header.php';
+        ?>
+        <!--Header end-->
+        
+        <div class="container">
+               <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <table class="table table-striped">
+
+                        <!--show table only if there are items added in the cart-->
+                        <?php
+                        $sum = 0;
+                        $user_id = $_SESSION['user_id'];
+                        $query = "SELECT items.price AS Price, items.id, items.name AS Name FROM users_items JOIN items ON users_items.item_id = items.id WHERE users_items.user_id='$user_id' and status='Added to cart'";
+                        $result = mysqli_query($con, $query)or die($mysqli_error($con));
+                        if (mysqli_num_rows($result) >= 1) {
+                            ?>
+                            <thead>
+                                <tr>
+                                    <th>Item Number</th>
+                                    <th>Item Name</th>
+                                    <th>Price</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                <?php
+                                while ($row = mysqli_fetch_array($result)) {
+                                    $sum+= $row["Price"];
+                                    $id="";
+                                    $id .= $row["id"] . ",";
+                                    echo "<tr>
+                                              <td>" . "#" . $row["id"] . "</td>
+                                              <td>" . $row["Name"] . "</td>
+                                              <td>Rs " . $row["Price"] . "</td>
+                                              <td><a href='cart-remove.php?id={$row['id']}' class='remove_item_link'> X </a></td>
+                                          </tr>";
+                                }
+                                $id = rtrim($id, ",");
+                                echo "<tr>
+                                          <td></td>
+                                          <td>Total</td>
+                                          <td>Rs " . $sum . "</td>
+                                          <td><a href='success.php?itemsid=".$id ."'class='btn btn-primary'>Confirm Order</a></td>
+                                          </tr>";
+                                ?>
+                            </tbody>
+                            <?php
+                        } else {
+                            echo "<center><h2>Add items to the cart first!</h2></center>";
+                        }
+                        ?>
+                        <?php
+                        ?>
+                    </table>
+                </div>
+             </div>
+        </div>
+        
+        <!--Footer-->
+        
+        <footer style="position: fixed;">
+    <div class="container">
+        <div class="row">
+            <div class="columnf">
+              <h3>Information</h3>
+                <p><a href="about_us.php"><span style="color:white">About Us</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">Contact Us</span></a></p>
+            </div>
+
+            <div class="columnf">
+              <h3>My Account</h3>
+                <p><a href="#" data-toggle="modal" data-target="#loginmodal" ><span style="color:white">Log In</span></a></p>
+                <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p>
+            </div>
+
+            <div class="columnf">
+              <h3>Contact Us</h3>
+                <p>CONTACT No.&nbsp;&nbsp;&nbsp;&nbsp;&#43;91&#45;123&#45;456&#45;7890</p>
+                <p>E&#45;Store&#46;com &copy;<?php echo DATE('Y') ;?>.&nbsp;&nbsp;All Rights Reserved.</p>
+                <a class="link" href="">Made with&nbsp;</a>&#x1f495;<a class="link" href="">&nbsp;in India</a>
+            </div>
+        </div>
+        
+    </div>
+</footer>
+        
+        <!--Footer end-->
+    </body>
+</html>
